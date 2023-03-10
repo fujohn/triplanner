d5684205ca976bf17cd9f0d7f313de0c8297d800
John Fu <fu.johnyh@gmail.com>
2023-03-09 20:10:00 -0800

    user app created

--- triplanner/apps/users/urls.py
+++ triplanner/apps/users/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('user/create', views.create), # POST
+    path('user/registered', views.register),
+    path('user/login', views.login) # POST
+
 ]
--- triplanner/apps/users/views.py
+++ triplanner/apps/users/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
     return render(request, 'login.html')
+
+# Registering
+def create(request):
+    # add create element
+    name = request.POST['name']
+    email = request.POST['email']
+    password = request.POST['password']
+    confirm_pw = request.POST['confirm_pw']
+
+    return redirect('/user/registered')
+
+def register(request):
+    return render(request, 'registered.html')
+
+# Logging in
+def login(request):
+    # read login and validate with existing data
+    email = request.POST['email']
+    password = request.POST['password']
+    return redirect('/trips')
