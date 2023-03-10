1d29b1dbf3214bd4f474feb2d8d5dd0822053fe6
John Fu <fu.johnyh@gmail.com>
2023-03-09 18:35:17 -0800

    sample

--- triplanner/apps/users/views.py
+++ triplanner/apps/users/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("this is the equivalent of @app.route('/')!")
+    return render(request, 'login.html')
